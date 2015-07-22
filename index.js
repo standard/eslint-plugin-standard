b612d5b406184386d71af20142d82eb4c06d3299
Jamund Ferguson <jamuferguson@paypal.com>
2015-07-22 16:50:36 -0700

    additional rules (not yet modified) :

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var path = require("path");
 module.exports = {
   rules: require("requireindex")(path.resolve(__dirname, "rules")),
   rulesConfig: {
-	"object-curly-even-spacing": 0
+    "object-curly-even-spacing": 0,
+    "computed-property-even-spacing": 0,
+    "array-bracket-even-spacing": 0
   }
 };
 
