49bee33b970434d8d18d4682f625b5e537dd8c9d
Jamund Ferguson <jamuferguson@paypal.com>
2015-07-23 09:13:29 -0700

    remove config for non-existent rule

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
   rules: require('requireindex')(path.resolve(__dirname, 'rules')),
   rulesConfig: {
     'object-curly-even-spacing': 0,
-    'computed-property-even-spacing': 0,
     'array-bracket-even-spacing': 0
   }
 }
