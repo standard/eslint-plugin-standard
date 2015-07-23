decc45910411ae1d53791e073ca03f13812d5bf7
Jamund Ferguson <jamuferguson@paypal.com>
2015-07-23 09:44:08 -0700

    New rule: computed-property-even-spacing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
   rules: require('requireindex')(path.resolve(__dirname, 'rules')),
   rulesConfig: {
     'object-curly-even-spacing': 0,
-    'array-bracket-even-spacing': 0
+    'array-bracket-even-spacing': 0,
+    'computed-property-even-spacing': 0
   }
 }
