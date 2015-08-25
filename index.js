7b872a2ee0f30dbbd259d911f932aaa2ac29491d
Dan Flettre <fletd01@yahoo.com>
2015-08-25 12:53:25 -0500

    avoid dynamic requires to enable browserify support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
-var path = require('path')
-
 module.exports = {
-  rules: require('requireindex')(path.resolve(__dirname, 'rules')),
+  rules: {
+    'array-bracket-even-spacing': require('./rules/array-bracket-even-spacing.js'),
+    'computed-property-even-spacing': require('./rules/computed-property-even-spacing.js'),
+    'object-curly-even-spacing': require('./rules/object-curly-even-spacing.js')
+  },
   rulesConfig: {
     'object-curly-even-spacing': 0,
     'array-bracket-even-spacing': 0,
